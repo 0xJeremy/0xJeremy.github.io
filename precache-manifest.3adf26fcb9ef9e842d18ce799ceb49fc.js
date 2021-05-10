1bd7546b9ced1610029319dcb46a56a3eab4472e
0xJeremy <jeremy.kanovsky@tufts.edu>
2021-05-10 18:36:29 -0400

    Updates

---
+++ precache-manifest.3adf26fcb9ef9e842d18ce799ceb49fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d52b28d4389833d38b8c71170833789",
+    "url": "/0xJeremy.github.io/index.html"
+  },
+  {
+    "revision": "9782557b1663760b2716",
+    "url": "/0xJeremy.github.io/static/js/2.91a686b0.chunk.js"
+  },
+  {
+    "revision": "b1cf8857018de83f14f35e442377e2b9",
+    "url": "/0xJeremy.github.io/static/js/2.91a686b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9db9cff54b9c829dce46",
+    "url": "/0xJeremy.github.io/static/js/main.3a15746e.chunk.js"
+  },
+  {
+    "revision": "4f413f6729b789f8a880",
+    "url": "/0xJeremy.github.io/static/js/runtime-main.0d206124.js"
+  }
+]);
